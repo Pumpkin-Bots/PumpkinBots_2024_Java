1815f4b3f28c378ed8a32fb3e4f77876a93eb4fa
TylerHandel <152944621+TylerHandel@users.noreply.github.com>
2024-05-27 13:52:52 -0700

    motor ports constants updated

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,15 +20,15 @@ import edu.wpi.first.wpilibj.TimedRobot;
  */
 public final class Constants {
   public static final class DriveConstants {
-    public static final int kFrontLeftDriveMotorPort = 0;
-    public static final int kRearLeftDriveMotorPort = 2;
-    public static final int kFrontRightDriveMotorPort = 4;
+    public static final int kFrontLeftDriveMotorPort = 8;
+    public static final int kRearLeftDriveMotorPort = 9;
+    public static final int kFrontRightDriveMotorPort = 7;
     public static final int kRearRightDriveMotorPort = 6;
 
-    public static final int kFrontLeftTurningMotorPort = 1;
-    public static final int kRearLeftTurningMotorPort = 3;
-    public static final int kFrontRightTurningMotorPort = 5;
-    public static final int kRearRightTurningMotorPort = 7;
+    public static final int kFrontLeftTurningMotorPort = 3;
+    public static final int kRearLeftTurningMotorPort = 4;
+    public static final int kFrontRightTurningMotorPort = 2;
+    public static final int kRearRightTurningMotorPort = 1;
 
     public static final int[] kFrontLeftTurningEncoderPorts = new int[] {0, 1};
     public static final int[] kRearLeftTurningEncoderPorts = new int[] {2, 3};
